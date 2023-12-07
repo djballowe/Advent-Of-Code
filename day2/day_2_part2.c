8624dc4fba15e1e8d95d8602e0bf7c642939071f
djballowe <djballowe@gmail.com>
2023-12-06 17:22:24 -0800

    day 2 part 2

--- day2/day_2_part2.c
+++ day2/day_2_part2.c
@@ -8,18 +8,19 @@ int convert(char* p, int l);
 int main(void) {
     char* buffer;
     long file_size;
-    FILE* file = fopen("test.txt", "rb");
+    FILE* file = fopen("input.txt", "rb");
+
+    char holder;
 
     int total = 0;
 
-    int id = 0;
-    int id_count = 2;
-    char p[3] = { 0, 0, 0 };
-    char holder;
-
     int red = 0;
-    int blue = 0;
     int green = 0;
+    int blue = 0;
+    int min_red = 0;
+    int min_green = 0;
+    int min_blue = 0;
+    int power = 0;
     char v[2] = { 0, 0 };
     int v_count = 1;
 
@@ -31,22 +32,10 @@ int main(void) {
     
     fread(buffer, 1, file_size, file);
 
-    fclose(file);
     buffer[file_size] = '\0';
+    fclose(file);
 
     for (int i = 0; i < file_size; i++) {
-        if (buffer[i] == ':') {
-            int j = i - 1;
-            holder = buffer[j]; 
-            while(isdigit(holder)) {
-                p[id_count] = holder;
-                id_count--;
-                j--;
-                holder = buffer[j];
-            }
-            id = convert(p, 3);
-            id_count = 2;
-        }
         if ((buffer[i] == 'r' && buffer[i - 1] != 'g') || buffer[i] == 'g' || buffer[i] == 'b') {
             int j = i - 2;
             holder = buffer[j];
@@ -59,27 +48,33 @@ int main(void) {
             buffer[i] == 'r' ? red = convert(v, 2) : buffer[i] == 'g' ? green = convert(v, 2) : buffer[i] == 'b' ? blue = convert(v, 2) : 0;
             v_count = 1;
         } 
-        if (red > 12 || green > 13 || blue > 14) {
-            printf("%d\n", red);
-            id = 0;
-        }
+
         if (buffer[i] == ';') {
+            min_red = red > min_red ? red : min_red;
+            min_blue = blue > min_blue ? blue : min_blue;
+            min_green = green > min_green ? green : min_green;
             red = 0;
             blue = 0;
             green = 0;
         }
+        
         if (buffer[i] == '\n') {
-            printf("id: %d is valid\n", id);
-            total += id;
+            min_red = red > min_red ? red : min_red;
+            min_blue = blue > min_blue ? blue : min_blue;
+            min_green = green > min_green ? green : min_green;
+            power = min_red * min_green * min_blue;
+            total += power;
             red = 0;
             blue = 0;
             green = 0;
+            min_red = 0;
+            min_green = 0;
+            min_blue = 0;
         }
     } 
     printf("%d\n", total);
     free(buffer);
 }
-
 
 int convert(char* p, int l) {
     int result = 0;
