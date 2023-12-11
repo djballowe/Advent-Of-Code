050ad38650a4af3080a115289ef182b516df300b
djballowe <djballowe@gmail.com>
2023-12-10 21:09:05 -0800

    day 3 part 2 90% done

--- day3/day3PartTwo.js
+++ day3/day3PartTwo.js
@@ -1,8 +1,5 @@
 import { readFileSync } from 'fs';
 
-let total = 0;
-let len = 0;
-let curNum = '';
 const main = () => {
     let file = parse().split(/\r?\n/);
     for (let i = 0; i < file.length; i++) {
@@ -14,30 +11,60 @@ const main = () => {
             //}
             if (file[i][j] === '*') {
                 let len = 2;
-                let count = 0;
-                let num = '';
-                while (count <= len) {
-                    if (file[i - 1] && !isNaN(file[i - 1][j - (count - 1)])) {
-                        // while (!isNaN(file[i - 1][j - (count - 1)])) {
-                           //  count++;
-                        // }
-                        console.log(file[i - 1][j - (count - 1)]);
+                let position = 0;
+                let adjacent = new Set();
+                let top = isNum(file[i - 1][j - (position - 1)]);
+                let bottom = isNum(file[i + 1][j - (position - 1)]);
+                let right = isNum(file[i][j + 1]);
+                let left = isNum(file[i][j - 1]);
+                while (position <= len && adjacent.size < 2) {
+                    if (file[i - 1] && top) {
+                        let num = getNum(file, -1, position, j, i);
+                        adjacent.add(num);
                     }
-                    curNum = '';
-                    count++;
+                    if (file[i + 1] && bottom) {
+                        let num = getNum(file, 1, position, j, i);
+                        adjacent.add(num);
+                    }
+                    if (right) {
+                        let num = getNum(file, 0, 0, j, i);
+                        adjacent.add(num);
+                    } else if (left) {
+                        let num = getNum(file, 0, 2, j, i);
+                        adjacent.add(num);
+                    }
+                    position++;
                 }
-                count = 0;
+                console.log(adjacent)
+                adjacent.clear();
+                position = 0;
             }
         }
     }
 };
 
-// 534001 low
-
 const parse = () => {
     return readFileSync('./test.txt', 'utf8', (data) => {
         return data.toString();
     });
+};
+
+const isNum = (num) => {
+    return !isNaN(parseInt(num));
+};
+
+const getNum = (file, orientation, position, j, i) => {
+    let counter = position;
+    let num = '';
+    while (isNum(file[i + orientation][j - (counter - 1)])) {
+        counter++;
+    }
+    let start = j - (counter - 1) + 1;
+    while (isNum(file[i + orientation][start])) {
+        num += file[i + orientation][start];
+        start++;
+    }
+    return parseInt(num);
 };
 
 main();
