e5013d06b8300cd1334c50bdefdb4310b6d23d31
david-ballowe-torus <david-ballowe@torus.co>
2023-12-11 20:30:48 -0700

    day 3 part 2 done

--- day3/day3PartTwo.js
+++ day3/day3PartTwo.js
@@ -2,22 +2,18 @@ import { readFileSync } from 'fs';
 
 const main = () => {
     let file = parse().split(/\r?\n/);
+    let total = 0;
     for (let i = 0; i < file.length; i++) {
         for (let j = 0; j < file[i].length; j++) {
-            let num = file[i][j];
-            // if (!isNaN(file[i][j])) {
-            //    curNum += num;
-            //   len++;
-            //}
             if (file[i][j] === '*') {
                 let len = 2;
                 let position = 0;
                 let adjacent = new Set();
-                let top = isNum(file[i - 1][j - (position - 1)]);
-                let bottom = isNum(file[i + 1][j - (position - 1)]);
-                let right = isNum(file[i][j + 1]);
-                let left = isNum(file[i][j - 1]);
-                while (position <= len && adjacent.size < 2) {
+                while (position <= len) {
+                    let top = isNum(file[i - 1][j - (position - 1)]);
+                    let bottom = isNum(file[i + 1][j - (position - 1)]);
+                    let right = isNum(file[i][j + 1]);
+                    let left = isNum(file[i][j - 1]);
                     if (file[i - 1] && top) {
                         let num = getNum(file, -1, position, j, i);
                         adjacent.add(num);
@@ -29,22 +25,30 @@ const main = () => {
                     if (right) {
                         let num = getNum(file, 0, 0, j, i);
                         adjacent.add(num);
-                    } else if (left) {
+                    }
+                    if (left) {
                         let num = getNum(file, 0, 2, j, i);
                         adjacent.add(num);
                     }
                     position++;
                 }
-                console.log(adjacent)
+                if (adjacent.size === 2) {
+                    let product = 1;
+                    adjacent.forEach((value) => {
+                       product *= value
+                    })
+                    total += product;
+                }
                 adjacent.clear();
                 position = 0;
             }
         }
     }
+    console.log(total)
 };
 
 const parse = () => {
-    return readFileSync('./test.txt', 'utf8', (data) => {
+    return readFileSync('./input.txt', 'utf8', (data) => {
         return data.toString();
     });
 };
@@ -66,5 +70,8 @@ const getNum = (file, orientation, position, j, i) => {
     }
     return parseInt(num);
 };
+
+// 323347 low
+// 83805819 low
 
 main();
