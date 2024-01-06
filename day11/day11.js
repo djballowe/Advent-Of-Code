d0162590d790332ebc9f2635d6528873ef280a80
djballowe <djballowe@gmail.com>
2024-01-06 09:44:34 -0800

    day 11 part 1

--- day11/day11.js
+++ day11/day11.js
@@ -3,13 +3,13 @@ import { readFileSync } from 'fs';
 const galaxy = '#';
 
 const buffer = () => {
-    return readFileSync('./test.txt', 'utf8', (data) => {
+    return readFileSync('./input.txt', 'utf8', (data) => {
         return data.toString();
     });
 };
 
 const expand = (file) => {
-    let space = [];
+    let rows = [];
     // expand rows
     for (let i = 0; i < file.length; i++) {
         let empty = true;
@@ -18,22 +18,44 @@ const expand = (file) => {
                 empty = false;
             }
         }
-        console.log(file[i]);
         if (empty) {
-            console.log(file[i]);
-            space.push(file[i]);
+            rows.push(file[i]);
         }
-        space.push(file[i]);
+        rows.push(file[i]);
     }
     // expand cols
     let i = 0;
     let j = 0;
-    while (j < space[0].length) {
-        console.log(`%c${space[i][j]}`, 'display: inline')
-        if (i++ === space.length - 1) {
+    let empty = true;
+    let cols = [];
+    while (j < rows[0].length) {
+        if (rows[i][j] === galaxy) {
+            empty = false;
+        }
+        if (i++ === rows.length - 1) {
+            if (empty) {
+                cols.push(j);
+            }
             i = 0;
             j++;
+            empty = true;
         }
+    }
+    let space = [];
+    for (let i = 0; i < rows.length; i++) {
+        let newRow = [];
+        for (let j = 0; j < rows[i].length; j++) {
+            cols.forEach((col) => {
+                if (j === col) {
+                    newRow.push('.')
+                }
+            })
+            newRow.push(rows[i][j])
+        }
+        space.push(newRow.join(''))
+    }
+    for (let i = 0; i < space.length; i++) {
+        console.log(space[i]);
     }
     return space;
 };
@@ -68,7 +90,7 @@ const findPath = (pair, space) => {
     console.log(pair);
     let start = { node: pair[0], distance: 0 };
     let target = pair[1];
-    let seen = new Set(`${start.node[0]}${start.node[1]}`);
+    let seen = new Set(`${start.node[0]},${start.node[1]}`);
     let queue = [start];
 
     while (queue.length) {
@@ -92,25 +114,26 @@ const findPath = (pair, space) => {
                 node[0] < space.length &&
                 node[1] >= 0 &&
                 node[1] < space[0].length &&
-                !seen.has(`${node[0]}${node[1]}`)
+                !seen.has(`${node[0]},${node[1]}`)
             ) {
-                seen.add(`${node[0]}${node[1]}`);
+                seen.add(`${node[0]},${node[1]}`);
                 queue.push({ node: node, distance: distance + 1 });
             }
         });
     }
-    return -1
+    throw new Error('No distance found')
 };
 
 const main = () => {
     let file = buffer()
         .split(/\r?\n/)
-        .filter((x) => x.length);
+        .filter((x) => x.length)
     let space = expand(file);
     let pairs = getPairs(space);
     let ans = 0;
-    //pairs.forEach((pair) => (ans += findPath(pair, space)));
-    console.log(ans);
+    pairs.forEach((pair) => (ans += findPath(pair, space)));
+    console.log(ans)
 };
+
 
 main();
