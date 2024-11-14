65a9f2c0151808be5b7539c7ce3cf89b84786876
David Ballowe <djballowe@gmail.com>
2024-11-13 20:54:30 -0800

    day1 part1

--- 2022/day1/day1.cpp
+++ 2022/day1/day1.cpp
@@ -1,18 +1,28 @@
 #include "iostream"
+#include <algorithm>
 #include <fstream>
 #include <string>
 
 int main() {
-    std::fstream file("day1-test.txt");
+    std::fstream file("day1.txt");
     if (!file) {
         std::cerr << "Unable to read file";
         return 1;
     }
 
     std::string line;
+    int count = 0;
+    int largest = 0;
     while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+        if (line == "") {
+            largest = std::max(largest, count);
+            count = 0;
+        } else {
+            count += std::stoi(line);
+        }
     }
+
+    std::cout << largest << std::endl;
 
     file.close();
     return 0;
