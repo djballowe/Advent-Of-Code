cce25d7ad17a2dc31651cc39a36ebe0f6ba8ae2a
djballowe <djballowe@gmail.com>
2024-12-03 23:22:16 -0800

    day 4 part 2 almost done

--- 2024/day4/day4.cpp
+++ 2024/day4/day4.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 // Part1
-
 bool wordSearch(int currY, int currX, int dirx, int diry, vector<string> &grid) {
     string word = "XMAS";
     int rows = grid[0].size();
@@ -56,6 +55,20 @@ void day4(string path) {
 //
 
 // Part 2
+bool liveMas(int currY, int currX, int dirx, int diry, vector<string> &grid, vector<char> &letters) {
+    int rows = grid[0].size();
+    int cols = grid.size();
+
+    int x = currX + 1 * dirx;
+    int y = currY + 1 * diry;
+    if (x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 'X' || grid[x][y] == 'A') {
+        return false;
+    }
+
+    letters.push_back(grid[x][y]);
+    return true;
+}
+
 void day4Part2(string path) {
     fstream file(path);
     if (!file) {
@@ -69,6 +82,35 @@ void day4Part2(string path) {
         grid.push_back(line);
     }
 
+    vector<pair<int, int>> directions = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+    vector<char> letters;
+    char target = 'A';
+    int ans = 0;
+    bool valid = true;
+
+    for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[i].size(); j++) {
+            if (grid[i][j] == target) {
+                for (const auto &[dirx, diry] : directions) {
+                    valid = liveMas(i, j, dirx, diry, grid, letters);
+                    if (!valid) {
+                        letters.clear();
+                        break;
+                    }
+                }
+                if (letters.size()) {
+                    for (char letter : letters) {
+                        cout << letter;
+                    }
+                    letters.clear();
+                    cout << endl;
+                }
+            }
+        }
+    }
+
+    cout << "Part 2: " << ans << endl;
+
     file.close();
     return;
 }
@@ -77,8 +119,8 @@ void day4Part2(string path) {
 int main() {
     string mainFile = "in.txt";
     string testFile = "in-test.txt";
-    day4(mainFile);
-    //    day4Part2(testFile);
+    // day4(mainFile);
+    day4Part2(testFile);
 
     return 0;
 }
