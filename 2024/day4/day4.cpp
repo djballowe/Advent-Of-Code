4c7cc244508777b6f6b5ef9cb4d5e133319ed87f
djballowe <djballowe@gmail.com>
2024-12-04 14:42:02 -0800

    day4 part 2

--- 2024/day4/day4.cpp
+++ 2024/day4/day4.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 
 // Part1
-bool wordSearch(int currY, int currX, int dirx, int diry, vector<string> &grid) {
+bool wordSearch(int currY, int currX, int dirX, int dirY, vector<string> &grid) {
     string word = "XMAS";
     int rows = grid[0].size();
     int cols = grid.size();
 
     for (int i = 0; i < word.size(); i++) {
-        int x = currX + i * dirx;
-        int y = currY + i * diry;
+        int x = currX + i * dirX;
+        int y = currY + i * dirY;
         if (x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] != word[i]) {
             return false;
         }
@@ -41,8 +41,8 @@ void day4(string path) {
 
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
-            for (const auto &[dirx, diry] : directions) {
-                ans += wordSearch(i, j, dirx, diry, grid);
+            for (const auto &[dirX, dirY] : directions) {
+                ans += wordSearch(i, j, dirX, dirY, grid);
             }
         }
     }
@@ -55,17 +55,17 @@ void day4(string path) {
 //
 
 // Part 2
-bool liveMas(int currY, int currX, int dirx, int diry, vector<string> &grid, vector<char> &letters) {
+bool liveMas(int currY, int currX, int dirX, int dirY, vector<string> &grid, vector<char> &letters) {
     int rows = grid[0].size();
     int cols = grid.size();
 
-    int x = currX + 1 * dirx;
-    int y = currY + 1 * diry;
-    if (x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 'X' || grid[x][y] == 'A') {
+    int x = currX + dirX;
+    int y = currY + dirY;
+    if (x < 0 || x >= rows || y < 0 || y >= cols || grid[y][x] == 'X' || grid[y][x] == 'A') {
         return false;
     }
 
-    letters.push_back(grid[x][y]);
+    letters.push_back(grid[y][x]);
     return true;
 }
 
@@ -91,19 +91,18 @@ void day4Part2(string path) {
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
             if (grid[i][j] == target) {
-                for (const auto &[dirx, diry] : directions) {
-                    valid = liveMas(i, j, dirx, diry, grid, letters);
+                for (const auto &[dirX, dirY] : directions) {
+                    valid = liveMas(i, j, dirX, dirY, grid, letters);
                     if (!valid) {
                         letters.clear();
                         break;
                     }
                 }
                 if (letters.size()) {
-                    for (char letter : letters) {
-                        cout << letter;
+                    if (letters[0] != letters[3] && letters[1] != letters[2]) {
+                        ans++;
                     }
                     letters.clear();
-                    cout << endl;
                 }
             }
         }
@@ -119,8 +118,8 @@ void day4Part2(string path) {
 int main() {
     string mainFile = "in.txt";
     string testFile = "in-test.txt";
-    // day4(mainFile);
-    day4Part2(testFile);
+    day4(mainFile);
+    day4Part2(mainFile);
 
     return 0;
 }
