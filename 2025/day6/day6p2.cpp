12fb91bf1c74d5663c0793f6bf03528c4cda2246
djballowe <djballowe@gmail.com>
2025-12-13 10:14:18 -0800

    day 6p2

--- 2025/day6/day6p2.cpp
+++ 2025/day6/day6p2.cpp
@@ -1,67 +1,48 @@
 #include "../../helpers/helpers.h"
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
-
-std::vector<std::vector<int>> parseNumbers(std::vector<std::string> &input) {
-    std::vector<std::vector<int>> lines;
-    std::vector<int> numbers;
-    int temp;
-
-    for (int i = 0; i < input.size() - 1; i++) {
-        std::stringstream ss(input[i]);
-        while (ss >> temp) {
-            numbers.push_back(temp);
-        }
-        lines.push_back(numbers);
-        numbers.clear();
-    }
-
-    return lines;
-}
-
-std::vector<char> parseOperators(std::string operator_line) {
-    std::vector<char> operators;
-
-    for (char op : operator_line) {
-        if (op == '*' || op == '+') {
-            operators.push_back(op);
-        }
-    }
-
-    return operators;
-}
 
 int main() {
     std::string path = "../2025/day6/in.txt";
     std::vector<std::string> input = AdventHelpers::parseLines(path);
-
-    std::vector<std::vector<int>> parsed_input = parseNumbers(input);
-    std::vector<char> parsed_operators = parseOperators(input[input.size() - 1]);
+    std::string opperators = input[input.size() - 1];
+    input.pop_back();
 
     int i = 0;
     int j = 0;
     long long curr_problem = 0;
     long long ans = 0;
+    char op = opperators[j];
+    std::string curr_num = "";
 
     while (true) {
-        char op = parsed_operators[j];
-        int num = parsed_input[i][j];
-        if (op == '*') {
-            curr_problem = curr_problem == 0 ? curr_problem = num : curr_problem *= num;
-        } else {
-            curr_problem += num;
+        if (opperators[j] != ' ') {
+            op = opperators[j];
         }
-
-        if (i == parsed_input.size() - 1) {
-            ans += curr_problem;
-            if (j == parsed_input[i].size() - 1) {
+        char num = input[i][j];
+        if (num != ' ') {
+            curr_num += num;
+        }
+        if (i == input.size() - 1) {
+            if (curr_num != "") {
+                int int_num = std::stoi(curr_num);
+                if (op == '*') {
+                    curr_problem = curr_problem == 0 ? curr_problem = int_num : curr_problem *= int_num;
+                } else {
+                    curr_problem += int_num;
+                }
+            } else if (curr_num == "") {
+                ans += curr_problem;
+                curr_problem = 0;
+            }
+            if (j == input[i].size() - 1) {
+                ans += curr_problem;
                 break;
             }
             i = 0;
             j++;
-            curr_problem = 0;
+            curr_num = "";
         } else {
             i++;
         }
