e376b3992f33fa68ffd5c8b30ac8f8abeaa3ebb4
djballowe <djballowe@gmail.com>
2025-12-18 13:55:47 -0800

    day 7 part two kind of done

--- 2025/day7/day7p2.cpp
+++ 2025/day7/day7p2.cpp
@@ -7,14 +7,11 @@
 std::optional<std::pair<int, int>> checkNode(size_t i, size_t j, std::vector<std::string> &input, int &ans) {
     while (i < input.size() - 1) {
         if (input[i][j] == '^') {
-            input[i][j] = '*';
-            ans++;
             return {{i, j}};
-        } else if (input[i][j] == '*') {
-            break;
         }
         i++;
     }
+    ans++;
     return std::nullopt;
 }
 
@@ -27,34 +24,34 @@ std::pair<int, int> findStart(std::vector<std::string> &input) {
         i++;
     }
 
-    input[i][j] = '*';
     return {i, j};
 }
 
 int main() {
-    std::string path = "../2025/day7/in-test.txt";
+    std::string path = "../2025/day7/in.txt";
     std::vector<std::string> input = AdventHelpers::parseLines(path);
 
-    int ans = 0;
+    int ans = 1;
     int i = 0;
     int j = 0;
 
     std::pair<int, int> start = findStart(input);
 
-    std::vector<std::pair<int, int>> start_points = {start};
+    std::vector<std::pair<int, int>> stack = {start};
 
-    while (start_points.size()) {
-        i = start_points.back().first;
-        j = start_points.back().second;
-        start_points.pop_back();
+    while (stack.size()) {
+        i = stack.back().first;
+        j = stack.back().second;
+        stack.pop_back();
         std::optional<std::pair<int, int>> right_node = checkNode(i, j + 1, input, ans);
         std::optional<std::pair<int, int>> left_node = checkNode(i, j - 1, input, ans);
         if (right_node) {
-            start_points.push_back(*right_node);
+            stack.push_back(*right_node);
         }
         if (left_node) {
-            start_points.push_back(*left_node);
+            stack.push_back(*left_node);
         }
+        std::cout << stack.size() << std::endl;
     }
 
     for (std::string line : input) {
